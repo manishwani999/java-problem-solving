8dd8916dbefb3ddf26649de2b10497580641644b
manish wani <manishwani03@gmail.com>
2026-02-16 19:42:25 +0530

    Find last digit program

---
+++ numberProblems/FindLastDigit.java
@@ -0,0 +1,7 @@
+public class FindLastDigit {
+    public static void main(String[] args) {
+        int num = 123456;
+        int lastDigit = num % 10;
+        System.out.println("Last Digit : " + lastDigit);
+    }
+}
