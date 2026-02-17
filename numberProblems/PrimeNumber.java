4e32fcedfb45a13c9697b49f13d67da542b3d6aa
manish wani <manishwani03@gmail.com>
2026-02-17 21:32:26 +0530

    .class removed

--- numberProblems/PrimeNumber.java
+++ numberProblems/PrimeNumber.java
@@ -1,21 +1,21 @@
 public class PrimeNumber {
     public static void main(String[] args) {
-        int num = 5;
-        boolean flag = false;
-        if (num == 0 || num == 1) {
-            flag = true;
+        int n = 11;
+        boolean isPrime = false;
+        if (n == 0 || n == 1) {
+            isPrime = true;
         } else {
-            for (int i = 2; i <= num / 2; i++) {
-                if (num % i == 0) {
-                    flag = true;
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    isPrime = true;
                     break;
                 }
             }
         }
-        if (flag == false) {
-            System.out.println("prime");
+        if (!isPrime) {
+            System.out.println("Prime");
         } else {
-            System.out.println("not prime");
+            System.out.println("Not prime");
         }
     }
 }
