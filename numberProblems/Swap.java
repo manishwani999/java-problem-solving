fccf3c21297841957d726dd95a80cf9713b1c0eb
manish wani <manishwani03@gmail.com>
2026-02-17 13:55:09 +0530

    swap second way

--- numberProblems/Swap.java
+++ numberProblems/Swap.java
@@ -1,11 +1,24 @@
 public class Swap {
     public static void main(String[] args) {
+
+        // first way
+
         int a = 10;
         int b = 20;
         int temp = a;
         a = b;
         b = temp;
-        System.out.println("a = "+a);
-        System.out.println("b = "+b);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // second way
+
+        int c = 10;
+        int d = 20;
+        c = c + d;
+        d = c - d;
+        c = c - d;
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
     }
 }
