4035c1819171e13cffb70e48281c187dbc75ef34
manish wani <manishwani03@gmail.com>
2026-02-17 19:05:05 +0530

    add

--- numberProblems/FindMax.java
+++ numberProblems/FindMax.java
@@ -2,6 +2,15 @@
 
 public class FindMax {
     public static void main(String[] args) {
-        
+        int n = 12543;
+        int max = 0;
+        while (n != 0) {
+            int last = n % 10;
+            if (last > max) {
+                max = last;
+            }
+            n /= 10;
+        }
+        System.out.println(max);
     }
 }
